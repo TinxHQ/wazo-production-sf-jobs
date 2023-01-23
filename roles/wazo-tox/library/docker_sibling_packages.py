ff1eb08f013ed57936ad1db7554a842911e4a4d5
Jesse Sopel <jesse.sopel@wazo.io>
2023-01-23 15:32:28 -0500

    Use f-strings and dict comprehensions since we don't need to run this in Py27 anymore

--- roles/wazo-tox/library/docker_sibling_packages.py
+++ roles/wazo-tox/library/docker_sibling_packages.py
@@ -12,33 +12,32 @@ def get_package_name(tox_python, root):
     subprocess.check_output(
         [os.path.abspath(tox_python), 'setup.py', 'egg_info'],
         cwd=os.path.abspath(root))
-    top_level = glob.glob(
-        "{}/*.egg-info/top_level.txt".format(root))[0]
+    top_level = glob.glob(f"{root}/*.egg-info/top_level.txt")[0]
     with open(top_level) as f:
         return f.read().strip()
 
 
 def main():
     module = AnsibleModule(
-        argument_spec=dict(
-            services=dict(required=True, type='list'),
-            tox_envlist=dict(required=True, type='str'),
-            project_dir=dict(required=True, type='str'),
-            projects=dict(required=True, type='list'),
-        )
+        argument_spec={
+            'services': {'required': True, 'type': 'list'},
+            'tox_envlist': {'required': True, 'type': 'str'},
+            'project_dir': {'required': True, 'type': 'str'},
+            'projects': {'required': True, 'type': 'list'},
+        }
     )
     envlist = module.params['tox_envlist']
     project_dir = module.params['project_dir']
     projects = module.params['projects']
     services = module.params['services']
 
-    envdir = '{project_dir}/.tox/{envlist}'.format(
-        project_dir=project_dir, envlist=envlist)
+    envdir = f'{project_dir}/.tox/{envlist}'
     if not os.path.exists(envdir):
         module.exit_json(
-            changed=False, msg=("envdir does not exist, "
-                                "skipping docker compose customisation"))
-    tox_python = '{envdir}/bin/python'.format(envdir=envdir)
+            changed=False,
+            msg="envdir does not exist, skipping docker compose customisation",
+        )
+    tox_python = f'{envdir}/bin/python'
 
     if not services:
         package_name = get_package_name(tox_python, project_dir)
@@ -57,20 +56,14 @@ def main():
 
         package = get_package_name(tox_python, root)
         volumes.add(
-            "{root}/{package}:"
-            "/opt/venv/lib/python3.7/site-packages/{package}".format(
-                root=os.path.realpath(root), package=package
-            ))
+            f"{os.path.realpath(root)}/{package}:/opt/venv/lib/python3.7/site-packages/{package}"
+        )
         volumes.add(
-            "{root}/{package}:"
-            "/opt/venv/lib/python3.9/site-packages/{package}".format(
-                root=os.path.realpath(root), package=package
-            ))
+            f"{os.path.realpath(root)}/{package}:/opt/venv/lib/python3.9/site-packages/{package}"
+        )
 
     version = '3'
-    compose_file = (
-        '{project_dir}/integration_tests/assets/docker-compose.yml'.format(
-            project_dir=project_dir))
+    compose_file = f'{project_dir}/integration_tests/assets/docker-compose.yml'
     if os.path.exists(compose_file):
         with open(compose_file) as f:
             data = yaml.load(f)
@@ -79,17 +72,15 @@ def main():
     volumes = list(volumes)
     docker_compose_override = {
         'version': version,
-        'services': dict([
-            (service, {
-                'volumes': volumes
-            }) for service in services
-        ])
+        'services': {
+            service: {'volumes': volumes}
+            for service in services
+        }
     }
 
     docker_compose_override_contents = yaml.dump(docker_compose_override)
     docker_compose_override_file = (
-        "{project_dir}/docker-compose.integration.override.yaml".format(
-            project_dir=project_dir))
+        f"{project_dir}/docker-compose.integration.override.yaml")
 
     with open(docker_compose_override_file, "w") as f:
         f.write(docker_compose_override_contents)
